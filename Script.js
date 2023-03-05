b1a6587ff6307c934fad5368135eb42a9cc82afd
Sachintha Ranasinghe <91958758+Bhanuka98@users.noreply.github.com>
2023-03-06 02:49:15 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,6 @@
+// Add a click event listener to the button
+const clickHereBtn = document.querySelector('.click-here-btn');
+clickHereBtn.addEventListener('click', function() {
+  // Add your click event code here
+  console.log('You clicked the "Click Here" button!');
+});
